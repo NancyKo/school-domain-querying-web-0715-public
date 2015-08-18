00cf7ea4b0c74d60f4c701054a3250c6b0ee00e6
N K <konancys@gmail.com>
2015-08-17 23:30:43 -0400

    Adding insert, new_from_db and find_by_name methods for Courses

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,54 @@
+require 'pry'
 class Course
+attr_accessor :id, :name, :department_id
+	def initialize
+		@id = id
+		@name = name
+		@department_id = department_id
+	end
+
+	def self.create_table
+		sql = """CREATE TABLE IF NOT EXISTS courses (
+						id INTEGER PRIMARY KEY,
+						name TEXT, 
+						department_id INTEGER REFERENCES departments);"""
+		DB[:conn].execute(sql)
+	end
+
+	def self.drop_table
+		sql = """DROP TABLE IF EXISTS courses"""
+		DB[:conn].execute(sql)
+	end
+
+	def insert 
+		sql = """ INSERT INTO courses (name)
+		VALUES (?)"""
+		DB[:conn].execute(sql, name)
+		sql = """SELECT last_insert_rowid() FROM courses"""
+		@id = DB[:conn].execute(sql)
+    self.id = @id.length
+	end
+
+	def self.new_from_db(row)
+		new_course = new 
+		new_course.id = row[0]
+		new_course.name = row[1]
+		new_course
+	end
+
+	def self.find_by_name(name)
+		sql = """SELECT * FROM courses WHERE name = ?;"""
+		# binding.pry
+		results = DB[:conn].execute(sql, name)
+    results.map {|row| self.new_from_db(row) }.first
+	end
+
+	def self.find_all_by_department_id(department_id)
+		sql = """SELECT * FROM courses WHERE department_id = ?"
+		results	=	DB[:conn].execute(sql, department_id)
+		binding.pry
+		results.map {|row| self.new_from_db(row) }.first
+
+	end
 
 end
